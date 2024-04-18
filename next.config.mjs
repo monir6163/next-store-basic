7ab980876e52a234982c68b51ab1c246e0da88d3
netstermonir <monirhossain6163@gmail.com>
2024-04-18 22:51:21 +0600

    b

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    API_URL: "http://localhost:3000",
-    MONGODB_URL:
-      "mongodb+srv://pdf:eCkdJo1drSxeGSuQ@cluster0.r1nyd.mongodb.net/next-store?retryWrites=true&w=majority",
-  },
   images: {
-    domains: ["res.cloudinary.com"],
+    remotePatterns: [
+      {
+        hostname: "res.cloudinary.com",
+      },
+    ],
   },
 };
 
