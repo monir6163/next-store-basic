97b8ccdd0e0b8de951cb04bf29838e665c0504e2
netstermonir <monirhossain6163@gmail.com>
2024-05-11 13:02:10 +0600

    Create new Order with Stripe

---
+++ src/app/api/orders/webhook/route.js
@@ -0,0 +1,100 @@
+import Order from "@/backend/models/order";
+import { authOptions } from "@/lib/authOptions";
+import { getServerSession } from "next-auth";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+async function getCartItems(line_items) {
+  return new Promise((resolve, reject) => {
+    const cartItems = [];
+    line_items?.data?.forEach(async (item) => {
+      const product = await stripe.products.retrieve(item.price.product);
+      const productId = product.metadata.productId;
+      cartItems.push({
+        product: productId,
+        name: product.name,
+        price: item.price.unit_amount_decimal / 100,
+        quantity: item.quantity,
+        image: product.images[0],
+      });
+      if (cartItems.length === line_items.data.length) {
+        resolve(cartItems);
+      }
+    });
+  });
+}
+
+export async function POST(req, _res) {
+  try {
+    const authSession = await getServerSession(authOptions);
+
+    const rawBody = await req.text();
+    const headersList = headers();
+    const signature = headersList.get("stripe-signature");
+    const event = stripe.webhooks.constructEvent(
+      rawBody,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
+
+    if (event.type === "checkout.session.completed") {
+      const session = event.data.object;
+      const line_items = await stripe.checkout.sessions.listLineItems(
+        event.data.object.id
+      );
+      const orderItems = await getCartItems(line_items);
+      const userId = session.client_reference_id;
+      const amountPaid = session.amount_total / 100;
+
+      const paymentInfo = {
+        id: session.payment_intent,
+        status: session.payment_status,
+        amountPaid,
+        taxPaid: session.total_details.amount_tax / 100,
+      };
+
+      const orderData = {
+        user: userId,
+        shippingInfo: session.metadata.shippingInfo,
+        paymentInfo,
+        orderItems,
+      };
+
+      // Save order to database
+      const order = await Order.create(orderData);
+      if (!order) {
+        return NextResponse.json(
+          {
+            status: false,
+            error: "Order not created",
+            hints: "Error saving order to database",
+            traceId:
+              Math.random().toString(36).substring(7) + "order-not-created",
+          },
+          { status: 500 }
+        );
+      }
+      return NextResponse.json(
+        {
+          status: true,
+          message: "Order created successfully",
+          order,
+        },
+        { status: 200 }
+      );
+    }
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      {
+        status: false,
+        error: error.message,
+        hints: "stripe route error",
+        traceId: Math.random().toString(36).substring(7) + "stripe-route-error",
+      },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/backend/models/order.js
@@ -0,0 +1,74 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+  {
+    shippingInfo: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: [true, "Shipping required Order Table"],
+      ref: "Address",
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: [true, "User required Order table"],
+      ref: "User",
+    },
+    orderItems: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: [true, "Product required order table"],
+          ref: "Product",
+        },
+        name: {
+          type: String,
+          required: [true, "Name is required"],
+        },
+        quantity: {
+          type: String,
+          required: [true, "qty is required"],
+        },
+        image: {
+          type: String,
+          required: [true, "Image Url is required"],
+        },
+        price: {
+          type: String,
+          required: [true, "Price is required"],
+        },
+      },
+    ],
+    paymentInfo: {
+      id: {
+        type: String,
+        required: true,
+      },
+      status: {
+        type: String,
+        required: true,
+      },
+      taxPaid: {
+        type: Number,
+        required: true,
+      },
+      amountPaid: {
+        type: Number,
+        required: true,
+      },
+      orderStatus: {
+        type: String,
+        default: "processing",
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+const Order = mongoose.models.Order || mongoose.model("Order", orderSchema);
+
+export default Order;
